1dac6ea197af09a92dd6bd67f28e83de8b42fcb0
jaehun <jaehun330@gmail.com>
2025-08-05 17:20:42 +0900

    첫번째 커밋

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -13,7 +13,3 @@ class _HomeScreenState extends State<HomeScreen> {
     return const Scaffold(body: Center(child: Text('Home')));
   }
 }
-
-//비지니스 로직은 일단 비워두고 화면 구현 우선
-// provider - notifier 공부해보고 상태관리 적용하기 (중요)
-// Theme 폴더에서 컬러, 텍스트 스타일 일괄 적용
--- lib/main.dart
+++ lib/main.dart
@@ -21,3 +21,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+//비지니스 로직은 일단 비워두고 화면 구현 우선
+// provider - notifier 공부해보고 상태관리 적용하기 (중요)
+// Theme 폴더에서 컬러, 텍스트 스타일 일괄 적용
