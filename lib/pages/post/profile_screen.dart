293e9b5c8bf5c77a6ce0c10a6060a87a476cd4a6
shusi1625 <snakes0625@gmail.com>
2025-08-09 13:06:58 +0900

    재빈 수영 합본

--- lib/pages/post/profile_screen.dart
+++ lib/pages/post/profile_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../theme/app_colors.dart';
+import '../../theme/app_colors.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
